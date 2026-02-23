cb39a0085eea1766040f2a945a81b57988d1043e
unknown <you@example.com>
2026-02-23 19:53:00 +0600

    my web

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ let rejectedList =[];
 let totalCount = document.getElementById('total')
 let interviweCount = document.getElementById('Interview')
 let rejectedCount =document.getElementById('rejected')
+
+const allTotalBtn = getElementById('all-btn');
+const allInterviewBtn =getElementById('all-interview');
+const allRejectedBtn = getElementById('all-rejected')
+
+
 const allcardsCount = document.getElementById("allcards")
+const mainCalculate = document.querySelector('main')
 
 function calculateCount (){
 totalCount.innerText =allcardsCount.children.length;
@@ -14,3 +21,19 @@ rejectedCount.innerText =rejectedList.length;
 
 }
 calculateCount()
+
+function toggleStyle(id){
+     allTotalBtn.classList.add('bg-white,text-white')
+    allInterviewBtn.classList.remove('bg-white' ,'text-gray')
+    allRejectedBtn.classList. remove('bg-white' ,'text-gray')
+
+
+    allTotalBtn.classList.remove('bg-[#3B82F6]' ,'text-white')
+    allInterviewBtn.classList.remove('bg-[#3B82F6]' ,'text-white')
+    allRejectedBtn.classList. remove('bg-[#3B82F6]' ,'text-white')
+    console.log(id)
+    const selected = document.getElementById(id)
+    console.log(selected)
+
+    selected.classList.remove
+}
